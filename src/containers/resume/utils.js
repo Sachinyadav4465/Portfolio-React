39633eb261c58ed8743a418bad6decb514e6d055
Sachinyadav4465 <yadavsachin4465@gmail.com>
2024-08-09 23:06:22 +0530

    React-Portfolio

---
+++ src/containers/resume/utils.js
@@ -0,0 +1,31 @@
+export const data = {
+    experience: [
+        {
+            title: "Consultant",
+            subtitle: "Miami,FL",
+            description: "User Experience,Visual Design"
+        },
+        {
+            title: "Junior Consultant",
+            subtitle: "London,UK",
+            description: "Visual Design"
+        },
+        {
+            title: "internship",
+            subtitle: "Delhi",
+            description: "Web developement,Database,Backend developement"
+        },
+    ],
+    education: [
+        {
+            title: "Secondary",
+            subtitle: "St Xavier High School",
+            description: "10th "
+        },
+        {
+            title: " Higher Secondary",
+            subtitle: "St Xavier High School",
+            description: "12th Grade in Science "
+        },
+    ],
+};
